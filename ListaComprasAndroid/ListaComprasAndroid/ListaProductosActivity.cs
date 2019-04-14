81dab120508cf95e1243f24bedc93f5692f6ad3a
Ivo Dias <igd753@gmail.com>
2019-04-14 15:52:32 -0300

    Linkedin Course

---
+++ ListaComprasAndroid/ListaComprasAndroid/ListaProductosActivity.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Newtonsoft.Json;
+
+namespace ListaComprasAndroid
+{
+    [Activity(Label = "ListaProductosActivity")]
+    public class ListaProductosActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Recebe os dados da outra tela
+            var items = JsonConvert.DeserializeObject<List<string[]>>(Intent.GetStringExtra("data"));
+            // Verifica os valores recebidos no terminal
+            Console.WriteLine(items[0][0]);
+            Console.WriteLine(items[0][1]);
+            // Cria um novo Layout
+            var layout = new LinearLayout(this);
+            layout.Orientation = Orientation.Vertical;
+            // Exibe os dados
+            for (int i = 0; i<items.Count; i++)
+            {
+                // Cria um layout de apresentacao
+                var layoutH = new LinearLayout(this);
+                layoutH.Orientation = Orientation.Horizontal;
+                // Cria a representacao do produto
+                var txtProducto = new TextView(this);
+                txtProducto.Text = items[i][0];
+                txtProducto.SetWidth(500);
+                txtProducto.SetPadding(0, 5, 5, 0);
+                txtProducto.SetTextSize(Android.Util.ComplexUnitType.Sp, 24);
+                txtProducto.SetTypeface(null, Android.Graphics.TypefaceStyle.Bold);
+                txtProducto.SetTextColor(Color.ParseColor("#ffff00"));
+                // Cria a representacao da quantidade
+                var txtCantidad = new TextView(this);
+                txtCantidad.Text = items[i][1];
+                txtCantidad.SetWidth(500);
+                txtCantidad.SetPadding(0, 5, 5, 0);
+                txtCantidad.SetTextSize(Android.Util.ComplexUnitType.Sp, 24);
+                // Configura exibicao
+                layoutH.AddView(txtProducto);
+                layoutH.AddView(txtCantidad);
+                // Configura tela principal
+                layout.AddView(layoutH);
+            }
+            // Mostra na tela
+            SetContentView(layout);
+        }
+    }
+}
---
+++ ListaComprasAndroid/ListaComprasAndroid/MainActivity.cs
@@ -0,0 +1,49 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System.Collections.Generic;
+using Android.Content;
+using Newtonsoft.Json;
+
+namespace ListaComprasAndroid
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        // Cria a variavel para salvar a lista
+        public List<string[]> miProducto = new List<string[]>(); 
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            // Cria a representacao do botao
+            var btn = FindViewById<Button>(Resource.Id.btnAgregar);
+
+            // Chama evento do Botao
+            btn.Click += Btn_Click;
+        }
+        // Evento de clicar no botao
+        private void Btn_Click(object sender, System.EventArgs e)
+        {
+            // Cria a representacao das TXTs
+            var producto = FindViewById<EditText>(Resource.Id.txtProducto);
+            var cantidad = FindViewById<EditText>(Resource.Id.txtCantidad);
+            // Recebe os valores
+            string[] data = new string[] { producto.Text, cantidad.Text };
+            // Salva eles
+            miProducto.Add(data);
+            // Limpa as TXTs
+            producto.Text = "";
+            cantidad.Text = "";
+            // Conecta com a outra tela
+            var intent = new Intent(this, typeof(ListaProductosActivity));
+            // Envia as informacoes
+            intent.PutExtra("data", JsonConvert.SerializeObject(miProducto));
+            // Abre a nova tela
+            StartActivity(intent);
+        }
+    }
+}
